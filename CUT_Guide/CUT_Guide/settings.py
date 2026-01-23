3562a22d2b64619c6fd6a879aef8fd54d576fdfc
Brian679 <brianchinyanga5@gmail.com>
2026-01-22 19:27:35 -0500

    edited the settings.py

--- CUT_Guide/CUT_Guide/settings.py
+++ CUT_Guide/CUT_Guide/settings.py
@@ -33,7 +33,7 @@ allowed_hosts_env = os.environ.get('DJANGO_ALLOWED_HOSTS', '')
 if allowed_hosts_env:
     ALLOWED_HOSTS = [h.strip() for h in allowed_hosts_env.split(',') if h.strip()]
 else:
-    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+    ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cutguide.pythonanywhere.com', '*']
 
 
 # ------------------------------------------------------------
